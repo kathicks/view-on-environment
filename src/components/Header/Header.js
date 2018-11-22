232745311766a411324803cde001bebcf8f882ca
kathicks <kmhicks92@gmail.com>
2018-11-22 21:41:22 +0000

    Add header component
    ∆ Remove boilerplate stylesheet and test files for App.js.
    ∆ Remove logo from create-react-app.
    ∆ Add custom font.
    ∆ Add initial styling for header.

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => 
+<header className="header">
+    <h1 className="header__title">View on the environment</h1>
+</header>
+
+export default Header
